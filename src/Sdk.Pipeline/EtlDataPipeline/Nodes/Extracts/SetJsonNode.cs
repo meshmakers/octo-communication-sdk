043cb7880d580250aafa644cb4daee98c8d0b819
Markus Roider <markus.roider@meshmakers.io>
2026-06-23 17:43:14 +0200

    AB#4229: feat: enhance SetJsonNode to support JSON path for dynamic string retrieval (#3)

--- src/Sdk.Pipeline/EtlDataPipeline/Nodes/Extracts/SetJsonNode.cs
+++ src/Sdk.Pipeline/EtlDataPipeline/Nodes/Extracts/SetJsonNode.cs
@@ -10,14 +10,19 @@ namespace Meshmakers.Octo.Sdk.Common.EtlDataPipeline.Nodes.Extracts;
 public record SetJsonNodeConfiguration : TargetPathNodeConfiguration
 {
     /// <summary>
-    /// The json string to write to the current object
+    /// The JSON string to write to the current object
     /// </summary>
     [PropertyGroup("Data", 0)]
-    public required string JsonString { get; init; } = null!;
+    public string? JsonString { get; init; }
+    /// <summary>
+    /// The JSON path to a JSON string to write to the target path
+    /// </summary>
+    [PropertyGroup("Paths", 2, "jsonpath")]
+    public string? JsonStringPath { get; init; }
 }
 
 /// <summary>
-/// Sets a json string to the current object
+/// Sets a JSON string to the current object
 /// </summary>
 /// <param name="next">Next node in the pipeline</param>
 [NodeConfiguration(typeof(SetJsonNodeConfiguration))]
@@ -29,8 +34,20 @@ public class SetJsonNode(NodeDelegate next) : IPipelineNode
     {
         var c = nodeContext.GetNodeConfiguration<SetJsonNodeConfiguration>();
 
-        dataContext.Set<JsonNode?>(c.TargetPath, JsonNode.Parse(c.JsonString),
-            c.DocumentMode, c.TargetValueKind, c.TargetValueWriteMode);
+        if (c.JsonString != null)
+        {
+            dataContext.Set<JsonNode?>(c.TargetPath, JsonNode.Parse(c.JsonString),
+                c.DocumentMode, c.TargetValueKind, c.TargetValueWriteMode);
+        }
+        else if (c.JsonStringPath != null)
+        {
+            var jsonString = dataContext.Get<string>(c.JsonStringPath);
+            if (jsonString != null)
+            {
+                dataContext.Set<JsonNode?>(c.TargetPath, JsonNode.Parse(jsonString),
+                    c.DocumentMode, c.TargetValueKind, c.TargetValueWriteMode);
+            }
+        }
 
         return next(dataContext, nodeContext);
     }
